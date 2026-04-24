234985d1877827e33c6ab03b33c94485b14a75af
Maksim Masolygin <mmasolygin@yandex.ru>
2026-04-24 09:03:50 +0700

    feat: implement HomePage and pages for Router logic

--- src/App/pages/HomePage/HomePage.tsx
+++ src/App/pages/HomePage/HomePage.tsx
@@ -1,5 +1,39 @@
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router';
+
+import { useSessionQuery } from '@/hooks/queries/useSessionQuery';
+import { useAuthStore } from '@/store/useAuthStore';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    useAuthStore.getState().logout();
+    navigate('/auth', { replace: true });
+  };
+
+  const { data: user } = useSessionQuery();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+        width: '100%',
+      }}
+    >
+      <Typography variant="h5" sx={{ mt: 2, mb: 4 }}>
+        Привет пользователь {user?._id}!
+      </Typography>
+
+      <Button variant="contained" onClick={handleLogout}>
+        Выйти из аккаунта
+      </Button>
+    </Box>
+  );
 };
 
 export default HomePage;
--- src/components/FullScreenLoader/FullScreenLoader.tsx
+++ src/components/FullScreenLoader/FullScreenLoader.tsx
@@ -1,5 +1,20 @@
+import { Box, CircularProgress } from '@mui/material';
+
 const FullScreenLoader = () => {
-  return <div>FullScreenLoader</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+        width: '100%',
+      }}
+    >
+      <CircularProgress size={40} />
+    </Box>
+  );
 };
 
 export default FullScreenLoader;
